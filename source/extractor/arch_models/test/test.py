934375bc548c6cb022a5e5c1de7f2f4053a6cd2f
Scotcho <hafner.marcel@gmx.de>
2022-05-15 07:58:51 -0700

    add new zipkin test

---
+++ source/extractor/arch_models/test/test.py
@@ -0,0 +1,29 @@
+import json
+import unittest
+
+from ...arch_models.zipkin_trace import ZipkinTrace
+from ...arch_models.architecture_misim import ArchitectureMiSim
+
+
+class TestExporter(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+        
+    def test_Zipkin_MiSim(self):
+        model_file = str('./zipkin_trace.json')
+        model = ZipkinTrace(model_file, False)
+        if not model:
+            return
+        model_name = model_file[model_file.rfind('/') + 1:]
+        model_name = model_name[:model_name.rfind('.')]
+        arch = ArchitectureMiSim(model)
+        exportmodel = json.loads(arch.export())
+        for data in exportmodel["microservices"]:
+            self.assertIsNotNone(data["name"])
+            self.assertGreater(data["instances"], 0)
+            self.assertIsNotNone(data["patterns"])
+            self.assertIsNotNone(data["capacity"])
+            self.assertIsNotNone(data["operations"])
--- source/test.py
+++ source/test.py
@@ -1,6 +1,7 @@
 import unittest
 
 from extractor.test import *
+from extractor.arch_models.test.test import *
 
 
 if __name__ == '__main__':
