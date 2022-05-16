204cd7ef3095a5d130eacee4228c11993143c047
Scotcho <hafner.marcel@gmx.de>
2022-05-16 08:09:15 -0700

    add test: - input: jeager - ouput: misim

--- source/extractor/arch_models/test/test.py
+++ source/extractor/arch_models/test/test.py
@@ -1,6 +1,7 @@
 import json
 import unittest
 
+from ...arch_models.jaeger_trace import JaegerTrace
 from ...arch_models.zipkin_trace import ZipkinTrace
 from ...arch_models.architecture_misim import ArchitectureMiSim
 
@@ -16,6 +17,23 @@ class TestExporter(unittest.TestCase):
         # Source of example trace zipkin_trace.json: https://zipkin.io/pages/data_model.html
         model_file = str('./extractor/arch_models/test/zipkin_trace.json')
         model = ZipkinTrace(model_file, False)
+        if not model:
+            return
+        model_name = model_file[model_file.rfind('/') + 1:]
+        model_name = model_name[:model_name.rfind('.')]
+        arch = ArchitectureMiSim(model)
+        exportmodel = json.loads(arch.export())
+        for data in exportmodel["microservices"]:
+            self.assertIsNotNone(data["name"])
+            self.assertGreater(data["instances"], 0)
+            self.assertIsNotNone(data["patterns"])
+            self.assertIsNotNone(data["capacity"])
+            self.assertIsNotNone(data["operations"])
+
+    def test_Jaeger_MiSim(self):
+        # Source of example trace zipkin_trace.json: https://zipkin.io/pages/data_model.html
+        model_file = str('./extractor/arch_models/test/jaeger_trace.json')
+        model = JaegerTrace(model_file, False)
         if not model:
             return
         model_name = model_file[model_file.rfind('/') + 1:]
