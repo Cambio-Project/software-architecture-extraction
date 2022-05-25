4f0f288501c8364f14f839a96a4b7bb7069376a8
Gabriel Glaser <gabriel.glaser@web.de>
2022-05-25 19:31:28 +0200

    integrate interactive cli in model2arch

--- source/model2arch.py
+++ source/model2arch.py
@@ -1,16 +1,16 @@
 import pickle
 import argparse
 
-from extractor.controllers.analyzer import Analyzer
-from extractor.controllers.exporter import Exporter
-from extractor.controllers.validator import Validator
-from extractor.arch_models.architecture_resirio import Architecture
-from extractor.arch_models.architecture_misim import ArchitectureMiSim
-from extractor.arch_models.jaeger_trace import JaegerTrace
-from extractor.arch_models.misim_model import MiSimModel
-from extractor.arch_models.zipkin_trace import ZipkinTrace
-from util.parse import bool_from_string
-from extractor.controllers.jaeger_network_manager import JaegerNetworkManager
+from source.extractor.controllers.analyzer import Analyzer
+from source.extractor.controllers.exporter import Exporter
+from source.extractor.controllers.validator import Validator
+from source.extractor.arch_models.architecture_resirio import Architecture
+from source.extractor.arch_models.architecture_misim import ArchitectureMiSim
+from source.extractor.arch_models.jaeger_trace import JaegerTrace
+from source.extractor.arch_models.misim_model import MiSimModel
+from source.extractor.arch_models.zipkin_trace import ZipkinTrace
+from source.input import InteractiveMain
+from source.util.parse import bool_from_string
 
 
 def cli():
@@ -50,87 +50,94 @@ def cli():
     parser.add_argument('--lightweight', dest='lightweight', action='store_true', required=False,
                         help='Exports the graph without meta information.')
 
+    # Interactive command line input
+    parser.add_argument('--interactive', dest='user_wants_interactive_cli', action='store_true', required=False)
+
     args = parser.parse_args()
-    model = None
-    arch = None
-    model_file = ''
-    model_name = ''
 
-    # Transformation
-    if args.model:
-        model_file = args.model[0]
-        model = pickle.load(open(model_file, 'rb'))
-    elif args.misim:
-        model_file = args.misim[0]
-        model = MiSimModel(model_file)
-    elif args.jaeger:
-        model_file = args.jaeger[0]
-        model = JaegerTrace(model_file, args.multiple)
-    elif args.zipkin:
-        model_file = args.zipkin[0]
-        model = ZipkinTrace(model_file, args.multiple)
+    if args.user_wants_interactive_cli:
+        InteractiveMain.main()
+    else:
+        model = None
+        arch = None
+        model_file = ''
+        model_name = ''
 
-    if model:
-        model_name = model_file[model_file.rfind('/') + 1:]
-        model_name = model_name[:model_name.rfind('.')]
-        if args.export_architecture[0].lower() == 'misim':
-            arch = ArchitectureMiSim(model)
-        else:
-            arch = Architecture(model)
+        # Transformation
+        if args.model:
+            model_file = args.model[0]
+            model = pickle.load(open(model_file, 'rb'))
+        elif args.misim:
+            model_file = args.misim[0]
+            model = MiSimModel(model_file)
+        elif args.jaeger:
+            model_file = args.jaeger[0]
+            model = JaegerTrace(model_file, args.multiple)
+        elif args.zipkin:
+            model_file = args.zipkin[0]
+            model = ZipkinTrace(model_file, args.multiple)
 
-    # Validation
-    if args.validate_model and model:
-        success, exceptions = Validator.validate_model(model)
-        success &= model.valid
-        print('Validation of {} model: {} {}'.format(
-            model.type,
-            'Successful' if success else 'Failed',
-            '' if not exceptions else '\n- ' + '\n- '.join(map(str, exceptions))))
+        if model:
+            model_name = model_file[model_file.rfind('/') + 1:]
+            model_name = model_name[:model_name.rfind('.')]
+            if args.export_architecture[0].lower() == 'misim':
+                arch = ArchitectureMiSim(model)
+            else:
+                arch = Architecture(model)
 
-    if args.validate_architecture and arch:
-        success, exceptions = Validator.validate_architecture(arch)
-        print('Validation of architecture: {} {}'.format(
-            'Successful' if success else 'Failed',
-            '' if not exceptions else '\n- ' + '\n- '.join(map(str, exceptions))))
+        # Validation
+        if args.validate_model and model:
+            success, exceptions = Validator.validate_model(model)
+            success &= model.valid
+            print('Validation of {} model: {} {}'.format(
+                model.type,
+                'Successful' if success else 'Failed',
+                '' if not exceptions else '\n- ' + '\n- '.join(map(str, exceptions))))
 
-    # Analysis
-    if args.analyze_model and model:
-        model.hazards = Analyzer.analyze_model(model)
+        if args.validate_architecture and arch:
+            success, exceptions = Validator.validate_architecture(arch)
+            print('Validation of architecture: {} {}'.format(
+                'Successful' if success else 'Failed',
+                '' if not exceptions else '\n- ' + '\n- '.join(map(str, exceptions))))
 
-    # Export
-    if args.export_model:
-        if model_file:
-            pickle.dump(model, open(model_name + '_model_export.dat', 'wb+'))
+        # Analysis
+        if args.analyze_model and model:
+            model.hazards = Analyzer.analyze_model(model)
 
-    if args.export_architecture:
+        # Export
+        if args.export_model:
+            if model_file:
+                pickle.dump(model, open(model_name + '_model_export.dat', 'wb+'))
 
-        stop = False
+        if args.export_architecture:
 
-        if not model:
-            print('No model!')
-            stop = True
-        if not arch:
-            print('No architecture!')
-            stop = True
+            stop = False
 
-        if stop:
-            exit(1)
+            if not model:
+                print('No model!')
+                stop = True
+            if not arch:
+                print('No architecture!')
+                stop = True
 
-        export_type = 'js'
-        model_type = export_type
-        pretty_print = False
-        if args.export_architecture[0].lower() == 'json':
-            export_type = 'json'
+            if stop:
+                exit(1)
+
+            export_type = 'js'
             model_type = export_type
-        elif args.export_architecture[0].lower() == 'misim':
-            export_type = 'json'
-            model_type = 'MiSim'
-        if len(args.export_architecture) > 1 and bool_from_string(args.export_architecture[1]):
-            pretty_print = True
+            pretty_print = False
+            if args.export_architecture[0].lower() == 'json':
+                export_type = 'json'
+                model_type = export_type
+            elif args.export_architecture[0].lower() == 'misim':
+                export_type = 'json'
+                model_type = 'MiSim'
+            if len(args.export_architecture) > 1 and bool_from_string(args.export_architecture[1]):
+                pretty_print = True
 
-        handle = open('{}_architecture_export.{}'.format(model_name, export_type), 'w+')
-        handle.write(Exporter.export_architecture(arch, model_type, pretty_print, args.lightweight))
-        handle.close()
+            handle = open('{}_architecture_export.{}'.format(model_name, export_type), 'w+')
+            handle.write(Exporter.export_architecture(arch, model_type, pretty_print, args.lightweight))
+            handle.close()
 
 
 if __name__ == '__main__':
