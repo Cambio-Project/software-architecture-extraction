466fc0db3a228f832b45bc5aebc77138dca63fe5
Gabriel Glaser <gabriel.glaser@web.de>
2022-05-26 18:33:13 +0200

    test change file path

--- source/extractor/arch_models/test/test.py
+++ source/extractor/arch_models/test/test.py
@@ -1,5 +1,6 @@
 import json
 import unittest
+import os
 
 from extractor.arch_models.jaeger_trace import JaegerTrace
 from extractor.arch_models.zipkin_trace import ZipkinTrace
@@ -15,7 +16,7 @@ class TestExporter(unittest.TestCase):
 
     def test_Zipkin_MiSim(self):
         # Source of example trace zipkin_trace.json: https://zipkin.io/pages/data_model.html
-        model_file = 'zipkin_trace.json'
+        model_file = os.path.join('source', 'extractor', 'arch_models', 'test', 'zipkin_trace.json')
         model = ZipkinTrace(model_file, False)
         if not model:
             return
@@ -32,7 +33,7 @@ class TestExporter(unittest.TestCase):
 
     def test_Jaeger_MiSim(self):
         # Source of example setup used for jaeger_trace.json: https://github.com/orcas-elite/example-setups
-        model_file = 'jaeger_trace.json'
+        model_file = os.path.join('source', 'extractor', 'arch_models', 'test', 'jaeger_trace.json')
         model = JaegerTrace(model_file, False)
         if not model:
             return
