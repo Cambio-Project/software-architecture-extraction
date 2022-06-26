5417efdd76bd0b025396255c0d42a99fa7ca8bd3
Gabriel Glaser <gabriel.glaser@web.de>
2022-06-26 02:57:42 +0200

    finish parsing of librede files

--- source/extractor/r_d_e/librede_caller.py
+++ source/extractor/r_d_e/librede_caller.py
@@ -13,8 +13,9 @@ from extractor.r_d_e.librede_output_parser import LibredeOutputParser
 # Creates the input-Files for LibReDE: response-times-.csv-Files, cpu-utilization-.csv-Files and the LibReDE_configuration-File.
 # Calls LibReDE.
 # Extracts the necessary information out of the output of LibReDE.
-# Returns a mapping between each operation (name) and its demand.
+# Adds the demands in the generic model.
 def calculate_and_add_demands_with_librede(model: IModel):
+    print("Start calculating resource-demands...")
     path_to_librede_installation: str = input("Path to your LibReDE-installation (e.g. \"C:\\Users\\Max\\Downloads\\librede\"): ")
     librede_bat_file: str = path_to_librede_installation + "\\tools.descartes.librede.releng.standalone\\target\\standalone\\console\\librede.bat"
 
@@ -28,18 +29,20 @@ def calculate_and_add_demands_with_librede(model: IModel):
     os.system(full_command)
 
     librede_output_parser = LibredeOutputParser(librede_input_creator.operations_on_host, path_to_librede_files + "output\\")
-    results: dict[LibredeServiceOperation, float] = librede_output_parser.get_results_of_librede()
-    for operation in results.keys():
-        add_demand_to_operation(operation, results[operation], model)
+    results_of_librede: dict[LibredeServiceOperation, float] = librede_output_parser.get_results_of_librede()
+    for service_operation in results_of_librede.keys():
+        add_demand_to_operation(service_operation, results_of_librede[service_operation])
 
 
-def add_demand_to_operation(librede_service_operation: LibredeServiceOperation, demanded_utilization: float, model: IModel):
+# Sets the demand of the given operation.
+def add_demand_to_operation(librede_service_operation: LibredeServiceOperation, demanded_utilization: float):
     service: Service = librede_service_operation.service
     operation: Operation = get_operation(librede_service_operation.operation_name, service)
-    demand: int = int(demanded_utilization * service.capacity)
+    demand = int(demanded_utilization * service.capacity)
     operation.set_demand(demand)
 
 
+# Searches for the Operation object in the service.
 def get_operation(operation_name_to_look_for: str, service: Service) -> Operation:
     for operation_name in service.operations.keys():
         if operation_name_to_look_for == operation_name:
--- source/extractor/r_d_e/librede_output_parser.py
+++ source/extractor/r_d_e/librede_output_parser.py
@@ -1,27 +1,38 @@
-from extractor.r_d_e.librede_host import LibredeHost
 from extractor.r_d_e.librede_service_operation import LibredeServiceOperation
 
 
+# Class which parses the output of LibReDE.
+# Will calculate the final utilization_demand as the average of all approaches.
 class LibredeOutputParser:
-    def __init__(self, services: list[LibredeServiceOperation], path_to_output_files: str):
-        self.services = services
+
+    def __init__(self, service_operations: list[LibredeServiceOperation], path_to_output_files: str):
+        self.service_operations = service_operations
         self.path_to_output_files = path_to_output_files
+        self.names_of_approaches = ["ResponseTimeApproximationApproach", "ServiceDemandLawApproach", "WangKalmanFilterApproach", "ZhangKalmanFilterApproach"]
 
+    # Calculates the mapping between the LibReDEServiceOperation and its demand_utilization.
     def get_results_of_librede(self) -> dict[LibredeServiceOperation, float]:
-        resource_demands = dict[LibredeServiceOperation, float]()
-        approach_results = self.get_approach_results()  # mapping between approach and result
-        # TODO transform results (result for every (host, service)-tuple) into a demand for every single host. (Sum of demands or average?)
-        return resource_demands
+        demanded_utilizations = dict[LibredeServiceOperation, float]()
+        results_of_approaches = self.parse_output_of_librede()
+        # Calculates the average result of all approaches
+        for service_operation in self.service_operations:
+            sum_of_results = 0
+            for approach_name in self.names_of_approaches:
+                approach_result = results_of_approaches[approach_name]
+                sum_of_results += approach_result[service_operation.host.id + service_operation.id]
+            demanded_utilizations[service_operation] = sum_of_results / len(self.names_of_approaches)
+        return demanded_utilizations
 
-    def get_approach_results(self) -> dict[str, list[float]]:
-        approaches = ["ResponseTimeApproximationApproach", "ServiceDemandLawApproach", "WangKalmanFilterApproach", "ZhangKalmanFilterApproach"]
-        approach_results = dict[str, list[float]]()  # mapping between approach to its results
-        for approach in approaches:
-            output_file_handler = open(self.path_to_output_files + "estimates_" + approach + "_fold_0.csv")
-            output_file_content = output_file_handler.read()
-            estimations_as_str = output_file_content.split(",")
+    # Retrieves the data from the output-csv-files of LibReDE and stores them in a mapping between approach and
+    # its estimations for every service-operation for every host.
+    def parse_output_of_librede(self) -> dict[str, list[float]]:
+        results_of_approaches = dict[str, list[float]]()  # mapping between approach_name and its results.
+        for approach_name in self.names_of_approaches:
+            output_file_handler = open(self.path_to_output_files + "estimates_" + approach_name + "_fold_0.csv")
+            output_file_content: str = output_file_handler.read()
+            estimations_as_str: list[str] = output_file_content.split(",")
             estimations = list[float]()
-            for i in range(1, len(estimations_as_str)):
+            for i in range(1, len(estimations_as_str)):  # Don't start at 0, because 0 is a timestamp.
                 estimations.append(float(estimations_as_str[i]))
-            approach_results[approach] = estimations
-        return approach_results
+            results_of_approaches[approach_name] = estimations
+        return results_of_approaches
