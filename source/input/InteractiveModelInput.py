1b37501a2b7dc2312612ca24bb91ac97f569a1b5
Gabriel <st168543@studi.uni-stuttgart.de>
2022-08-07 16:56:54 +0200

    sleight change of input of previously created extractions

--- source/input/InteractiveModelInput.py
+++ source/input/InteractiveModelInput.py
@@ -1,4 +1,7 @@
-from input.input_utils import get_valid_string_input_with_finite_valid_options_and_descriptions, get_valid_file_path_input
+import os.path
+
+from input.input_utils import get_valid_string_input_with_finite_valid_options_and_descriptions, \
+    get_valid_file_path_input, get_valid_string_input_with_predicates
 
 
 class InteractiveModelInput:
@@ -9,17 +12,21 @@ class InteractiveModelInput:
     def __init__(self):
         self.contains_generic_model = False
         self.contains_misim_model = False
-        self.ask_for_model_type()
-        if self.contains_model():
-            self.model_file_path = get_valid_file_path_input("Path to a " + ("Generic (Pickle) " if self.contains_generic_model else "MiSim") + "-model file")
+        self.model_file_path = None
+        self.ask_for_model()
 
-    def ask_for_model_type(self):
-        input_specification = "Do you want to enter a Generic model (Pickle) or MiSim model that has been created previously or no model?"
-        input_descriptions = ["g", "m", "Enter for no model"]
-        inputs = ["g", "m", ""]
-        model_answer = get_valid_string_input_with_finite_valid_options_and_descriptions(input_specification, input_descriptions, inputs)
-        self.contains_generic_model = model_answer == "g"
-        self.contains_misim_model = model_answer == "m"
+    def ask_for_model(self):
+        input_specification = "Optionally enter a previosuly created extraction:"
+        input_descriptions = [
+            "Path to extraction (either .dat-file for Generic Model (pickle) or .json for MiSim-architecture",
+            "Enter for no model"]
+        inputs_predicates = [os.path.isfile, lambda a: a == ""]
+        model_answer = get_valid_string_input_with_predicates(input_specification, input_descriptions,
+                                                              inputs_predicates)
+        if model_answer[0] == 0:
+            self.model_file_path = model_answer[1]
+            self.contains_generic_model = model_answer[1].endswith(".dat")
+            self.contains_misim_model = model_answer[1].endswith(".json")
 
     def contains_model(self):
         return self.contains_generic_model or self.contains_misim_model
@@ -30,6 +37,7 @@ class InteractiveModelInput:
     def print_summary_of_model_input(self):
         print("Model-input: ", end="")
         if self.contains_model():
-            print(("Generic" if self.contains_generic_model else "MiSim") + "-model at \"" + self.model_file_path + "\"")
+            print(
+                ("Generic" if self.contains_generic_model else "MiSim") + "-model at \"" + self.model_file_path + "\"")
         else:
             print("No model.")
